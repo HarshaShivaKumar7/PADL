5ee71292fe6e058fce0cd716d05099a76c498767
Harsha ShivaKumar <54899429+HarshaShivaKumar7@users.noreply.github.com>
2024-05-08 02:47:31 +0530

    08/05/2024

--- 4_led.py
+++ 4_led.py
@@ -1,17 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 17
+# GPIO pin setup
+GPIO.setmode(GPIO.BOARD)
+ldr_pin = 7  
+red_led_pin = 11 
+green_led_pin = 13 
+delay_time = 0.1
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN,GPIO.OUT)
+GPIO.setup(red_led_pin, GPIO.OUT)
+GPIO.setup(green_led_pin, GPIO.OUT)
+GPIO.output(red_led_pin, False)
+GPIO.output(green_led_pin, False)
+
+def read_ldr(ldr_pin):
+    count = 0
+    GPIO.setup(ldr_pin, GPIO.OUT)
+    GPIO.output(ldr_pin, False)
+    time.sleep(delay_time)
+    GPIO.setup(ldr_pin, GPIO.IN)
+    # Count until the pin goes high
+    while (GPIO.input(ldr_pin) == 0):
+        count += 1
+    return count
 
 try:
     while True:
-        GPIO.output(LED_PIN,GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(LED_PIN,GPIO.LOW)
-        time.sleep(1)
-        
+        ldr_value = read_ldr(ldr_pin)
+        print("LDR Value:", ldr_value)
+        if ldr_value > 10000:
+            print("Light is dim")
+            GPIO.output(red_led_pin, True)
+            GPIO.output(green_led_pin, False)
+        else:
+            print("Light is bright")
+            GPIO.output(red_led_pin, False)
+            GPIO.output(green_led_pin, True)
+        time.sleep(1)  # Delay for a second
+
 except KeyboardInterrupt:
+    pass
+finally:
     GPIO.cleanup()
